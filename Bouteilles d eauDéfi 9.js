8c7c3a3918ea34f277f147acce93ff0c51c93577
mrxSAW <arkkass2018@gmail.com>
2025-10-29 18:02:15 +0100

    chalange done

--- Bouteilles d eauDéfi 9.js
+++ Bouteilles d eauDéfi 9.js
@@ -1,8 +1,18 @@
 const prompt = require("prompt-sync")();
 
-let NpreBoutEau = 9
-let  ntotal=NpreBoutEau+(NpreBoutEau/3)
+let pleines = Number(prompt("Combien de bouteilles pleines au départ ? "));
+let bues = 0;
+let vides = 0;
 
+while (pleines > 0) {
+  bues += pleines;
+  vides += pleines;
+  pleines = 0;
 
-console.log(NpreBoutEau)
-console.log(ntotal)
+  
+  let echangeables = Math.floor(vides / 3);
+  pleines = echangeables;
+  vides = vides % 3; // Reste de bouteilles vides non échangeables
+}
+
+console.log("Tu peux boire au total :", bues, "bouteilles.");
